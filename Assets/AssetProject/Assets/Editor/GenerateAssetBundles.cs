b788dd14f940fcb3183e420331c35335411fff4c
AlexejheroYTB <alexejhero@gmail.com>
2020-01-10 00:56:19 +0200

    clsm - Add Assets 'n stuff

---
+++ Assets/AssetProject/Assets/Editor/GenerateAssetBundles.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using UnityEditor;
+
+public class ExportAssetBundles
+{
+    [MenuItem("Tools/Build AssetBundles")]
+    static void ExportResource()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        File.Delete("Assets/AssetBundles/AssetBundles");
+        File.Delete("Assets/AssetBundles/AssetBundles.meta");
+        File.Delete("Assets/AssetBundles/AssetBundles.manifest");
+        File.Delete("Assets/AssetBundles/AssetBundles.manifest.meta");
+    }
+}
---
+++ QModManager/HarmonyPatches/CustomLoadScreen.cs
@@ -0,0 +1,26 @@
+﻿
+namespace QModManager.HarmonyPatches.CustomLoadScreen
+{
+    using Harmony;
+    using System;
+    using UnityEngine;
+
+    [HarmonyPatch(typeof(uGUI_SceneLoading), nameof(uGUI_SceneLoading.Init))]
+    internal static class uGUI_SceneLoading_Init
+    {
+        internal static GameObject progressBar;
+
+        [HarmonyPrefix]
+        internal static void Prefix(uGUI_SceneLoading __instance)
+        {
+            AssetBundle bundle = AssetBundle.LoadFromMemory(Convert.FromBase64String(LoadingBarAssetBundle.data));
+            GameObject prefab = bundle.LoadAsset<GameObject>("Assets/Prefabs/LoadingBar.prefab");
+            GameObject obj = GameObject.Instantiate(prefab);
+        }
+
+        internal static class LoadingBarAssetBundle
+        {
+            public const data = "removed due to VS performance reasons :P";
+        }
+    }
+}
